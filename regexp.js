313454637e4ba576ec3e6e13ecc0da523d8e4f44
Marianna1903 <84705083+Marianna1903@users.noreply.github.com>
2021-10-07 18:58:01 +0400

    Add files via upload

---
+++ regexp.js
@@ -0,0 +1,59 @@
+// function f(x){
+// 	var reg = /^[A-Z]/;
+// 	if(x.match(reg)){
+// 		console.log("true");
+// 	}else{
+// 		console.log("false")
+// 	}
+
+// }
+// f("Hello");
+
+
+// let arr = ['goal','new','user','sit','eat','dinner'];
+// let reg = /e/;
+// for(var i=0;i<arr.length;i++){
+// 	if(!arr[i].match(reg)){
+// 		arr.splice(i,1)
+// 	}
+// }
+// console.log(arr);
+
+
+// let str = "They ate 5 apples and 5 oranges";
+// let reg = /5/;
+// for(var i=0;i<str.length;i++){
+// 	if(str[i].match(reg)){
+// 		str2 = str.replace("5","five");
+// 	}
+// }
+// console.log(str2);
+
+
+// let arr = ["lovely","fdentist","2 lonely","eden","flyn","dent"];
+// let reg = /^den|ly$/;
+// for(let i=0;i<arr.length;i++){
+// 	if(arr[i].match(reg)){
+// 		arr.splice(i,1);
+// 	}
+// }
+// console.log(arr);
+
+
+
+///////////????????
+// let str = "Grigor` (077)00-01-02, Mariam` (098)02-01-00, Armen` (077)01-02-00";
+// let reg = /(\(\d{3}\)(\d{2}-\d{2}-\d{2}))/;
+// document.write(str.match(reg))
+// console.log(str.match(reg))
+
+
+// let str = "www,site,com www,bigsite,org www,supersite,net";
+// let reg = /,/;
+// if(str.match(reg)){
+// 	var str1 = str.replaceAll(",",".");
+// }
+// console.log(str1)
+
+
+
